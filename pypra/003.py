77dd78f0d32b8864259c3201a6e828db885e1370
lihaimei <993101488@qq.com>
2018-03-14 23:17:19 +0800

    003xiufu

--- pypra/003.py
+++ pypra/003.py
@@ -3,7 +3,7 @@ import math
 
 num = 1
 while True:
-    if math.sqrt(num + num)-int(math.sqrt(num + 100))==0 and math.sqrt(num + 268)-int(math.sqrt(num + 268))==0:
+    if math.sqrt(num + 100)-int(math.sqrt(num + 100))==0 and math.sqrt(num + 268)-int(math.sqrt(num + 268))==0:
         print(num)
         break
     num += 1
