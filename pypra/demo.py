87fc3c9b4b73e19c213f11891a2e53c88c48f56f
lihaimei <993101488@qq.com>
2018-03-22 19:38:53 +0800

    0316

--- pypra/demo.py
+++ pypra/demo.py
@@ -1,5 +1,5 @@
-import urllib.request
+from urllib.request import urlopen
 
-request1 = urllib. request("http://www.baidu.com")
-response = urllib.urlopen(request1)  #urlopen(url,data,timeout)2参3参可不传
+
+response = urlopen('http://www.baidu.com ') #urlopen(url,data,timeout)2参3参可不传
 print (response.read())
