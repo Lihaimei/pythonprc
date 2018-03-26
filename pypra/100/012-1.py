2ddc1b314a737a2f6e17db7b6ebd34ff09799607
lihaimei <993101488@qq.com>
2018-03-26 20:21:06 +0800

    0316

--- pypra/100/012-1.py
+++ pypra/100/012-1.py
@@ -1,5 +1,13 @@
 #判断101-200之间有多少个素数，并输出所有素数
 #用101到200之间的数去整除2到该数前面的一个数；
-for i in range(101,201):
-    flag = 1
-    k = int(sqrt(i))
+
+b = 0#个数
+for i in range(101,200):
+    k = 0
+    for j in range(2,i):
+        if i%j ==0:
+            k +=1
+    if k == 0:
+            print(i)
+            b +=1
+print("素数一共有",b,"个")
--- pypra/100/012-2.py
+++ pypra/100/012-2.py
@@ -1,2 +1,14 @@
 #判断101-200之间有多少个素数，并输出所有素数
 #用101到200之间的数去整除2到sqrt（这个数）；
+import math
+b =0
+for i in range(101,200):
+    k = 0
+    a =int(math.sqrt(i))
+    for j in range(2,a):
+        if i%j ==0:
+            k +=1
+    if k ==0:
+        print(i);
+        b +=1
+print("素数个数：",b,"个")
