63eb027f0b666d1e242183be1339844e4402ec59
lihaimei <993101488@qq.com>
2018-03-26 19:04:24 +0800

    0316

--- pypra/demo.py
+++ pypra/demo.py
@@ -1,5 +1,12 @@
 from urllib.request import urlopen
-
-
-response = urlopen('http://www.baidu.com ') #urlopen(url,data,timeout)2参3参可不传
+values ={
+    "token":"a24becfad6d86c1492586cedc1e155c7",
+    "room_title":"13240的聊天室",
+    "city_name":"上海",
+    "platform":"",
+    "templet":"1",
+    "v_num":""
+}
+response = urlopen('http://192.168.26.18:10011v1/chatroom/create',values)
+#urlopen(url,data,timeout)2参3参可不传
 print (response.read())
