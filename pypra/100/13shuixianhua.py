c70bc942a9a016d05a169bf34cc5de56e40aebfd
lihaimei <993101488@qq.com>
2018-03-28 19:11:09 +0800

    0316

--- pypra/100/13shuixianhua.py
+++ pypra/100/13shuixianhua.py
@@ -1,10 +1,12 @@
 """打印出所有的“水仙花数”，所谓“水仙花数”是指一个三位数，其各位数字立方和等于该数本身。
 例如：153是一个“水仙花数”，因为153=1的三次方＋5的三次方＋3的三次方。"""
-
+import math
 for i in range(100,1000):
-    a = i
-    b = i%10
-    c = i%100
-    if i ==a**3+b**3+c**3:
-         print(i)
+    a = math.floor(i/100)
+    b = math.floor((i-a*100)/10)
+    c = i-math.floor(i/10)/10
+    if i == a**3 + b**3 + c**3:
+        print(i,end=',')
+print(i,end=',')
+
 
